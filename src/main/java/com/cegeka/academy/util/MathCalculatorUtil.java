2113d3f49381f61e6ffeb2e1624a3893f091d9f9
Bianca Virtopeanu <bianca-georgiana.virtopeanu@ing.ro>
2019-04-16 20:33:23 +0300

    fix 4 map

--- src/main/java/com/cegeka/academy/util/MathCalculatorUtil.java
+++ src/main/java/com/cegeka/academy/util/MathCalculatorUtil.java
@@ -9,10 +9,12 @@ public class MathCalculatorUtil {
     private static List<CalculateService> calculateServices = null; // TODO create a list with all CalculateService implementations
 
     public static Number calculateByStrategy(Strategy strategy, List<? extends Number> list) {
-        return calculateServices.stream()
-                .filter(service -> service.getStrategy().equals(strategy.name()))
-                .findFirst()
-                .map(calculateService -> calculateService.calculate(list))
-                .get();
+        for (CalculateService service: calculateServices) {
+            if(service.getStrategy().equals(strategy.name())){
+                return service.calculate(list);
+            }
+        }
+
+        throw new RuntimeException("you should not be here");
     }
 }
